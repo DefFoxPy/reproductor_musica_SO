c3ab37be1e10a9d0ec8d28f065c8f6a0a751ab39
Mint <juanmpy05@gmail.com>
2023-11-06 16:35:28 -0400

    Update main2.py

--- main2.py
+++ main2.py
@@ -2,7 +2,16 @@ import csv
 import time
 import sys
 import random
-from multiprocessing import Process
+from multiprocessing import Process, Lock
+
+# Constantes
+TIME_WRITE = 10  # lo que tarda en escribir los datos de una música
+
+# lo que tarda en volver a intentar agregar una música
+TIME_WAIT_MIN = 7
+TIME_WAIT_MAX = 15
+
+TIME_DISPLAY = 7  # Cada cuanto se mostrará las canciones por pantalla
 
 
 # Estructura de la canción
@@ -17,8 +26,19 @@ class Song:
 
 
 # Función para agregar una canción a la lista
-def add_song(song, song_list):
-    with open(song_list, "a", newline="") as file:
+def add_song(song, song_list_file, lock):
+    print(f"(>) {song.added_by} intenga agregar una música")
+    if not lock.acquire(block=False):
+        print("(!) Ya Existe un usuario que está agregando una canción")
+        return False
+    elif song_exists(song, song_list_file):
+        time.sleep(TIME_WRITE)
+        print(f"(-) La música '{song.title}' ya está agregada")
+        lock.release()
+        return False
+
+    time.sleep(TIME_WRITE)
+    with open(song_list_file, "a", newline="") as file:
         writer = csv.writer(file)
         writer.writerow(
             [
@@ -30,12 +50,14 @@ def add_song(song, song_list):
                 song.duration,
             ]
         )
+    lock.release()
+    return True
 
 
 # Función para mostrar el estado de la lista de canciones y las últimas dos ultimas
-def display_song_list(song_list):
+def display_song_list(song_list_file):
     while True:
-        with open(song_list, "r") as file:
+        with open(song_list_file, "r") as file:
             reader = csv.reader(file)
             try:
                 sonds = list(reader)
@@ -45,12 +67,12 @@ def display_song_list(song_list):
                     print(sond)
             except csv.Error as e:
                 sys.exit("archivo {}, linea {}: {}".format(file, reader.line_num, e))
-        time.sleep(5)
+        time.sleep(TIME_DISPLAY)
 
 
 # Función para verificar si una canción ya está en la lista
-def song_exists(song, song_list):
-    with open(song_list, "r") as file:
+def song_exists(song, song_list_file):
+    with open(song_list_file, "r") as file:
         reader = csv.reader(file)
         try:
             for i in reader:
@@ -61,18 +83,15 @@ def song_exists(song, song_list):
     return False
 
 
-def user_process(song_list, username, new_songs):
+def user_process(song_list_file, username, new_songs, lock):
     while True:
         new_song = random.choice(new_songs)
         new_song.added_by = username
 
         # Validar si la canción ya existe en la lista
-        if not song_exists(new_song, song_list):
-            add_song(new_song, song_list)
-        else:
-            print("La canción ya está en la lista.")
-
-        time.sleep(5)
+        if add_song(new_song, song_list_file, lock):
+            print(f"(+) Música '{new_song.title}' agregada por {new_song.added_by}")
+        time.sleep(random.randint(TIME_WAIT_MIN, TIME_WAIT_MAX))
 
 
 # Músicas de ejemplo
@@ -81,7 +100,7 @@ new_song1 = Song(
     "NOMA",
     "Cytus II-Neko",
     time.strftime("%Y-%m-%d"),
-    "Mint",
+    "x",
     "1:58",
 )
 
@@ -90,7 +109,7 @@ new_song2 = Song(
     "Toby Fox",
     "UNDERTALE Soundtrack",
     time.strftime("%Y-%m-%d"),
-    "Soul",
+    "x",
     "2:36",
 )
 
@@ -99,7 +118,7 @@ new_song3 = Song(
     "Laura Shigihara",
     "Plans Vs. Zombies",
     time.strftime("%Y-%m-%d"),
-    "Mint",
+    "x",
     "1:42",
 )
 
@@ -108,13 +127,15 @@ new_song4 = Song(
     "Ricardo Arjona",
     "Blanco",
     time.strftime("%Y-%m-%d"),
-    "Mint",
+    "x",
     "3:57",
 )
 
 if __name__ == "__main__":
     song_list_file = "song_list.csv"
     sounds_list = [new_song1, new_song2, new_song3, new_song4]
+
+    lock = Lock()
 
     p_display = Process(target=display_song_list, args=(song_list_file,))
     p_user1 = Process(
@@ -123,6 +144,7 @@ if __name__ == "__main__":
             song_list_file,
             "Juan",
             sounds_list,
+            lock,
         ),
     )
     p_user2 = Process(
@@ -131,13 +153,12 @@ if __name__ == "__main__":
             song_list_file,
             "Maria",
             sounds_list,
+            lock,
         ),
     )
 
-    p_display.start()
     p_user1.start()
     p_user2.start()
 
-    p_display.join()
     p_user1.join()
     p_user2.join()
